b35f711bd5adc7ce310f921e81c79fcf893b6cd4
SarahRex666 <95315055+SarahRex666@users.noreply.github.com>
2022-10-22 12:29:10 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :teachers
+  resources :dance_classes
+  resources :registrations
+  resources :users 
+  resources :sessions
+  resources :reviews
+  resources :passwords
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  post "/signup", to: "users#create"
+  post "/classreg", to: "registration#create"
+  delete "/logout", to: "sessions#destroy"
 end
